8958e33ba1de3a5d5772a704b3a6b3e93b5c0b06
Gian William Ramelb <144856637+rame0033@users.noreply.github.com>
2024-10-09 09:41:02 -0400

    Add display categories fn

--- week6/starter/main.js
+++ week6/starter/main.js
@@ -1,13 +1,35 @@
 // Variables
 // 1. Create a Categories Array
+const categories = ['All', 'T-Shirt', 'Hoodies'];
 
 
 /**
  * Displays the list of categories as HTML list items on the page.
  * 
- * /
+ */
 
 //2. Display Categories Dynamically
+// Create a function to display the categories | function
+function displayCategories(){
+
+    // Retrieve the HTML element where the categories will be displayed | getElementById
+    const categoriesEl=document.getElementById('categories');
+
+    // Create an empty list (array) to store the category elements | const, array
+    const tempArr=[];
+    // Loop through each item in the categories array | forEach
+    categories.forEach((category) => {
+        // Create an HTML list item for each category | template literal, push
+        tempArr.push(
+              `<li class="nav-item">
+              <a href="#" class="nav-link btn btn-light btn-sm rounded-0 mx-1">${category}</a></li>`
+            );
+    });
+    // Combine the list of category elements into a single string | join
+    // Insert the HTML into the categories element | innerHTML
+    categoriesEl.innerHTML = tempArr.join('');
+} // -> Test the function in the console.
+
 
 
 //3. Refactor with map()
